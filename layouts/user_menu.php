14ff83538073f4e92a7bb08f78ec494723d721fa
ManuelCanoFig <jcano@ucol.mx>
2018-12-13 12:44:06 -0600

    Project for manage inventory. MVC Model HTML,PHP,MYSQL,JAVASCRIPT

---
+++ layouts/user_menu.php
@@ -0,0 +1,29 @@
+<ul>
+  <li>
+    <a href="home.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Panel de control</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-list"></i>
+       <span>Prestamos</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="sales.php">Administar Prestamos</a> </li>
+         <li><a href="add_sale.php">Agregar Prestamos</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-signal"></i>
+       <span>Reporte de ventas</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="sales_report.php">Reporte por fecha </a></li>
+        <li><a href="monthly_sales.php">Reporte mensual</a></li>
+        <li><a href="daily_sales.php">Reporte diario</a> </li>
+      </ul>
+  </li>
+</ul>
